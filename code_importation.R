3d330ce5e211b61031ea27c250230cd00abb5407
Alexis-Barrau <alexis.barrau@ensae.fr>
2023-11-16 14:14:50 +0000

    Quelques retouches

--- code_importation.R
+++ code_importation.R
@@ -1,13 +1,14 @@
 #importation présidentielles 2022
 temp <- tempfile()
 download.file("https://conflit-politique-data.ams3.cdn.digitaloceanspaces.com/zip/pres2022_csv.zip", temp)
-dezip <- unzip(temp,"pres2022_csv/pres2022comm.csv")
-table <- read.csv2("/home/onyxia/work/pres2022_csv/pres2022comm.csv", sep = ",")   
+unzip(temp,"pres2022_csv/pres2022comm.csv")
+table <- read.csv2("/home/onyxia/work/statappensae/pres2022_csv/pres2022comm.csv", sep = ",") 
+rm(temp)
 
 #importation capitalimmobilier
 temp <- tempfile()
 download.file("https://conflit-politique-data.ams3.cdn.digitaloceanspaces.com/zip/Capital_immobilier_csv.zip", temp)
 list_fichier <- unzip(temp,list=TRUE)
 unzip(temp,"Capital_immobilier_csv/capitalimmobilier.csv")
-table <- read.csv2("/home/onyxia/work/Capital_immobilier_csv/capitalimmobilier.csv", sep = ",")
+table <- read.csv2("/home/onyxia/work/statappensae/Capital_immobilier_csv/capitalimmobilier.csv", sep = ",")
 rm(temp)  
