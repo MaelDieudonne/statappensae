8dd220cd4559681becd63bfb6ac19d3caac7886f
maeldieudonne <mael.dieudonne@ensae.fr>
2024-01-25 12:31:54 +0000

    Moved to data.table

--- 1981.r
+++ 1981.r
@@ -1,25 +1,44 @@
+# Librairies
 library(aws.s3)
 library(data.table)
-library(dplyr)
+# Environnement
+bucket_name <- "maeldieudonne"
 
 # Résultats électoraux
 ## Téléchargement
-pres1981 <- 
-  aws.s3::s3read_using(
-    FUN = data.table::fread,
-    object = "/diffusion/csv/Elections_pres/pres1981comm.csv",
-    bucket = "maeldieudonne",
-    opts = list("region" = "")
-  )
-## Recodages
-### Droite / gauche
-pres1981$GD_ratio_T1 <- (pres1981$voixLAGUILLER + pres1981$voixMARCHAIS + pres1981$voixMITTERRAND + pres1981$voixCREPEAU + pres1981$voixBOUCHARDEAU + pres1981$voixLALONDE) / pres1981$exprimes
-setnames(pres1981, old = "pvoixT2MITTERRANDratio", new = "GD_ratio_T2")
-### Libéralisme culturel / conservatisme moral
-pres1981$lib_ratio_T1 <- (pres1981$voixMITTERRAND + pres1981$voixGISCARDDESTAING + pres1981$voixBOUCHARDEAU + pres1981$voixLALONDE) / pres1981$exprimes
-## Nettoyage
-pres1981 <- pres1981 %>%
-  dplyr::select(-starts_with("pv"), -starts_with("vote"))
+object_key <- "/diffusion/Elections_pres/pres1981comm.feather"
+pres1981 <- aws.s3::s3read_using(
+  FUN = arrow::read_feather,
+  object = object_key,
+  bucket = bucket_name,
+  opts = list("region" = "")
+)
+pres1981 <- as.data.table(pres1981)
 
-variable_names <- names(pres1981)
-print(variable_names)
+candidats <- fread("candidats.csv", header = TRUE)
+
+# Recodages
+## Droite / gauche
+T1mask <- candidats[DrGau == "G", .(variable = paste0("voix", Candidat))]
+pres1981[, GD_ratio_T1 := rowSums(.SD, na.rm = TRUE) / exprimes, .SDcols = T1mask$variable]
+T2mask <- candidats[DrGau == "G", .(variable = paste0("voixT2", Candidat))]
+T2candidates <- intersect(T2mask$variable, names(pres1981))
+pres1981[, GD_ratio_T2 := rowSums(.SD, na.rm = TRUE) / exprimesT2, .SDcols = T2candidates]
+## Libéralisme culturel / conservatisme moral
+T1mask <- candidats[LibCons == "L", .(variable = paste0("voix", Candidat))]
+pres1981[, lib_ratio_T1 := rowSums(.SD, na.rm = TRUE) / exprimes, .SDcols = T1mask$variable]
+
+# Nettoyage
+prefixes_to_drop <- c("pvoix", "vote", "pvote", "pervote")
+pres1981 <- pres1981[, .SD, .SDcols = setdiff(names(pres1981), grep(paste0("^", paste(prefixes_to_drop, collapse = "|")), names(pres1981), value = TRUE))]
+
+# Propriétaires
+## Téléchargement
+object_key <- "/diffusion/Proprietaires/proprietairescommunes.feather"
+propri <- aws.s3::s3read_using(
+  FUN = arrow::read_feather,
+  object = object_key,
+  bucket = bucket_name,
+  opts = list("region" = "")
+)
+propri <- as.data.table(propri)
